7f79eda0d1cb1cee8a1b70d4dab62adab01a3aab
Usman Nadiry <usmannadiry.dev@gmail.com>
2026-06-24 16:51:58 +0500

    fix: add production config for sequelize-cli

--- config/sequelize-cli.cjs
+++ config/sequelize-cli.cjs
@@ -5,4 +5,14 @@ module.exports = {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
   },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
